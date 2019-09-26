a7e3fa26df777a1ac2f69d34e53082a32bbc8f8e
satyajitdey02 <satyajit02@gmail.com>
2019-09-26 23:56:39 +0600

    Add Question Form

--- pages/questions/ask.js
+++ pages/questions/ask.js
@@ -1,9 +1,180 @@
 import AppLayout from "./../../components/AppLayout";
+import RichTextEditor from './../../components/RichTextEditor';
+import { EditorState } from "draft-js";
 
-export default function Ask() {
-  return (
-      <AppLayout>
-       Question Page
-      </AppLayout>
-  );
-}
+import {
+    Form,
+    Input,
+    Tooltip,
+    Icon,
+    Cascader,
+    Select,
+    Row,
+    Col,
+    Checkbox,
+    Button,
+    AutoComplete,
+  } from 'antd';
+  
+  const { Option } = Select;
+  const AutoCompleteOption = AutoComplete.Option;
+  
+  const residences = [
+    {
+      value: 'zhejiang',
+      label: 'Zhejiang',
+      children: [
+        {
+          value: 'hangzhou',
+          label: 'Hangzhou',
+          children: [
+            {
+              value: 'xihu',
+              label: 'West Lake',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  
+  class RegistrationForm extends React.Component {
+    state = {
+      confirmDirty: false,
+      autoCompleteResult: [],
+    };
+  
+    handleSubmit = e => {
+      e.preventDefault();
+      this.props.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    };
+  
+    handleConfirmBlur = e => {
+      const { value } = e.target;
+      this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+  
+    compareToFirstPassword = (rule, value, callback) => {
+      const { form } = this.props;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords that you enter is inconsistent!');
+      } else {
+        callback();
+      }
+    };
+  
+    validateToNextPassword = (rule, value, callback) => {
+      const { form } = this.props;
+      if (value && this.state.confirmDirty) {
+        form.validateFields(['confirm'], { force: true });
+      }
+      callback();
+    };
+  
+    handleWebsiteChange = value => {
+      let autoCompleteResult;
+      if (!value) {
+        autoCompleteResult = [];
+      } else {
+        autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
+      }
+      this.setState({ autoCompleteResult });
+    };
+  
+    render() {
+      const { getFieldDecorator } = this.props.form;
+      const { autoCompleteResult } = this.state;
+  
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+      };
+      const tailFormItemLayout = {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        },
+      };
+      const prefixSelector = getFieldDecorator('prefix', {
+        initialValue: '86',
+      })(
+        <Select style={{ width: 70 }}>
+          <Option value="86">+86</Option>
+          <Option value="87">+87</Option>
+        </Select>,
+      );
+  
+      const websiteOptions = autoCompleteResult.map(website => (
+        <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+      ));
+  
+      return (
+          <AppLayout>
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <Form.Item label="Question Title">
+            {getFieldDecorator('title', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input title',
+                },
+              ],
+            })(<Input />)}
+          </Form.Item>
+          <Form.Item label="Question Body">
+            {getFieldDecorator('body', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Write question in details',
+                },
+              ],
+            })(<RichTextEditor editorState={new EditorState.createEmpty()}/>)}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Post Question
+            </Button>
+          </Form.Item>
+        </Form>
+        </AppLayout>
+      );
+    }
+  }
+  
+  const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
+
+  export default WrappedRegistrationForm;
+  
