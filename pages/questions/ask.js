d628b7a8deb157fc13e43a3eaa61e348ee4bd725
satyajitdey02 <satyajit02@gmail.com>
2019-09-26 23:58:33 +0600

    Remove unnecessary configs and validation code

--- pages/questions/ask.js
+++ pages/questions/ask.js
@@ -19,42 +19,9 @@ import {
   const { Option } = Select;
   const AutoCompleteOption = AutoComplete.Option;
   
-  const residences = [
-    {
-      value: 'zhejiang',
-      label: 'Zhejiang',
-      children: [
-        {
-          value: 'hangzhou',
-          label: 'Hangzhou',
-          children: [
-            {
-              value: 'xihu',
-              label: 'West Lake',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      value: 'jiangsu',
-      label: 'Jiangsu',
-      children: [
-        {
-          value: 'nanjing',
-          label: 'Nanjing',
-          children: [
-            {
-              value: 'zhonghuamen',
-              label: 'Zhong Hua Men',
-            },
-          ],
-        },
-      ],
-    },
-  ];
   
-  class RegistrationForm extends React.Component {
+  
+  class QuestionForm extends React.Component {
     state = {
       confirmDirty: false,
       autoCompleteResult: [],
@@ -72,23 +39,6 @@ import {
     handleConfirmBlur = e => {
       const { value } = e.target;
       this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-    };
-  
-    compareToFirstPassword = (rule, value, callback) => {
-      const { form } = this.props;
-      if (value && value !== form.getFieldValue('password')) {
-        callback('Two passwords that you enter is inconsistent!');
-      } else {
-        callback();
-      }
-    };
-  
-    validateToNextPassword = (rule, value, callback) => {
-      const { form } = this.props;
-      if (value && this.state.confirmDirty) {
-        form.validateFields(['confirm'], { force: true });
-      }
-      callback();
     };
   
     handleWebsiteChange = value => {
@@ -127,18 +77,8 @@ import {
           },
         },
       };
-      const prefixSelector = getFieldDecorator('prefix', {
-        initialValue: '86',
-      })(
-        <Select style={{ width: 70 }}>
-          <Option value="86">+86</Option>
-          <Option value="87">+87</Option>
-        </Select>,
-      );
+      
   
-      const websiteOptions = autoCompleteResult.map(website => (
-        <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
-      ));
   
       return (
           <AppLayout>
@@ -174,7 +114,7 @@ import {
     }
   }
   
-  const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
+  const WrappedQuestionForm = Form.create({ name: 'ask' })(QuestionForm);
 
-  export default WrappedRegistrationForm;
+  export default WrappedQuestionForm;
   
