7b1553ddc13c4683f26d620e27ba897a24379b72
satyajitdey02 <satyajit02@gmail.com>
2019-09-30 22:04:42 +0600

    Add tags

--- pages/questions/ask.js
+++ pages/questions/ask.js
@@ -6,11 +6,15 @@ import {stateToHTML} from 'draft-js-export-html';
 import {stateFromHTML} from 'draft-js-import-html';
 import AppLayout from './../../components/AppLayout';
 
+import { TweenOneGroup } from 'rc-tween-one';
+
+
 import fetch from 'isomorphic-unfetch';
 
 import {
     Form,
     Input,
+    Tag,
     Tooltip,
     Icon,
     Cascader,
@@ -34,11 +38,62 @@ import {
           editorState: null,
           showEditor: false,
           confirmDirty: false,
-          autoCompleteResult: []
+          autoCompleteResult: [],
+          tags: []
         };
         this.questionForm = React.createRef();
       }
     
+      handleClose = removedTag => {
+        const tags = this.state.tags.filter(tag => tag !== removedTag);
+        console.log(tags);
+        this.setState({ tags });
+      };
+    
+      showInput = () => {
+        this.setState({ inputVisible: true }, () => this.input.focus());
+      };
+    
+      handleInputChange = e => {
+        this.setState({ inputValue: e.target.value });
+      };
+    
+      handleInputConfirm = () => {
+        const { inputValue } = this.state;
+        let { tags } = this.state;
+        if (inputValue && tags.indexOf(inputValue) === -1) {
+          tags = [...tags, inputValue];
+        }
+        console.log(tags);
+        this.setState({
+          tags,
+          inputVisible: false,
+          inputValue: '',
+        });
+      };
+    
+      saveInputRef = input => (this.input = input);
+      forMap = tag => {
+        const tagElem = (
+          <Tag
+            closable
+            onClose={e => {
+              e.preventDefault();
+              this.handleClose(tag);
+            }}
+          >
+            {tag}
+          </Tag>
+        );
+        
+        return (
+          <span key={tag} style={{ display: 'inline-block' }}>
+            {tagElem}
+          </span>
+        );
+      };
+  
+        
       onChange = (currentContent) => {
         const blocks = currentContent.blocks;
         const value = blocks.map(block => (!block.text.trim() && '\n') || block.text).join('\n');
@@ -68,6 +123,7 @@ import {
       this.props.form.validateFieldsAndScroll((err, values) => {
         if (!err) {
          // console.log('Received values of form: ', values);
+         values.tags  = [...this.state.tags];
           this.postQuestions(values);
         }
       });
@@ -108,6 +164,9 @@ import {
     render() {
       const { getFieldDecorator } = this.props.form;
       const { autoCompleteResult } = this.state;
+
+      const { tags, inputVisible, inputValue } = this.state;
+      const tagChild = tags.map(this.forMap);
   
       const formItemLayout = {
         labelCol: {
@@ -162,6 +221,44 @@ import {
                   editorClassName="editorClassName"
                   onEditorStateChange={this.onEditorStateChange}
                 />)}
+          </Form.Item>
+          <Form.Item>
+          <div>
+        <div style={{ marginBottom: 16 }}>
+          <TweenOneGroup
+            enter={{
+              scale: 0.8,
+              opacity: 0,
+              type: 'from',
+              duration: 100,
+              onComplete: e => {
+                e.target.style = '';
+              },
+            }}
+            leave={{ opacity: 0, width: 0, scale: 0, duration: 200 }}
+            appear={false}
+          >
+            {tagChild}
+          </TweenOneGroup>
+        </div>
+        {inputVisible && (
+          <Input
+            ref={this.saveInputRef}
+            type="text"
+            size="small"
+            style={{ width: 78 }}
+            value={inputValue}
+            onChange={this.handleInputChange}
+            onBlur={this.handleInputConfirm}
+            onPressEnter={this.handleInputConfirm}
+          />
+        )}
+        {!inputVisible && (
+          <Tag onClick={this.showInput} style={{ background: '#fff', borderStyle: 'dashed' }}>
+            <Icon type="plus" /> New Tag
+          </Tag>
+        )}
+      </div>
           </Form.Item>
           <Form.Item {...tailFormItemLayout}>
             <Button type="primary" htmlType="submit">
